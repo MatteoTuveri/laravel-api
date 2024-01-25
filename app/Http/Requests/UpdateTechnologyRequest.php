8437c424fa00c6cccb703e83103adb9a7fb8d79e
MatteoTuveri <140177942+MatteoTuveri@users.noreply.github.com>
2024-01-25 09:37:45 +0100

    fix update

--- app/Http/Requests/UpdateTechnologyRequest.php
+++ app/Http/Requests/UpdateTechnologyRequest.php
@@ -23,7 +23,7 @@ class UpdateTechnologyRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'name' => ['required','min:3', 'max:200',Rule::unique('technology')->ignore($this->technology)],
+            'name' => ['required','min:3', 'max:200',Rule::unique('technologies')->ignore($this->technology)],
             'documentation' =>['nullable','url'],
             'icon' =>['nullable','image']
         ];
