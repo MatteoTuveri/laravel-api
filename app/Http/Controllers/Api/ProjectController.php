643a3f917aa75cc9757f1990c3b55f41a0534bc7
MatteoTuveri <140177942+MatteoTuveri@users.noreply.github.com>
2024-01-26 03:01:25 +0100

    Add Category controller and filter

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -8,12 +8,18 @@ use Illuminate\Http\Request;
 
 class ProjectController extends Controller
 {
-    public function index(){
-        $projects = Project::all();
-        return response()->json([
-            'success'=>true,
-            'projects' => $projects
-        ]);
+    public function index(Request $request){
+        if($request->query('category')){
+            $projects = Project::where('category_id', $request->query('category'))->paginate(2);
+        }else{
+            $projects = Project::paginate(2);
+        } 
+        return response()->json(
+            [
+                'success' => true,
+                'projects' => $projects
+            ]
+        );
     }
     public function show($slug){
         $project = Project::where('slug',$slug)->with(['category','technologies'])->first();
